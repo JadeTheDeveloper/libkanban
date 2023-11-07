ddb724a3620417cfd19c9bd15019c533dc2d458a
JadeTheDeveloper <cazextra@gmail.com>
2023-11-06 17:26:04 -0700

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,7 @@
+public class main {
+
+    //I Know having the class named main is a horrible practice. Don't change it.
+    public static void main(String[] args){
+        System.out.println("you made of water? Us too.");
+    }
+}
