243cbbafb35104ed4eeec21189441334594f50c2
JadeTheDeveloper <cazextra@gmail.com>
2023-11-06 17:45:39 -0700

    Added an initiation function for the window

--- src/main.java
+++ src/main.java
@@ -1,8 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+
 public class main {
+
+    JFrame frame = new JFrame();
+
+
+    public void initWindow(){
+        //sets everything with the layouts and all that jazz. This currently only works with 1920x1080 monitors. Sorry :P.
+        //frame.setSize(1920,1080);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
 
     //I Know having the class named main is a horrible practice. Don't change it.
     public static void main(String[] args){
-        System.out.println("you made of water? Us too.");
+        main instance = new main();
+        instance.initWindow();
     }
 
 }
