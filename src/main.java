57ac51516e692104eb37fe6f561f6d9f66233878
JadeTheDeveloper <cazextra@gmail.com>
2023-11-11 09:16:03 -0700

    Started top menu

--- src/main.java
+++ src/main.java
@@ -4,11 +4,22 @@ import java.awt.*;
 
 public class main {
 
-    JFrame frame = new JFrame();
+    JFrame frame = new JFrame("libkanban");
+    JMenuBar menuBar = new JMenuBar();
+    JMenu menu = new JMenu("File");
+    JMenuItem exit = new JMenuItem("Exit");
+
+    public void drawTopMenu(){
+        menu.add(exit);
+        menuBar.add(menu);
+        frame.setJMenuBar(menuBar);
+    }
+
 
 
     public void initWindow(){
         Color backgroundColour = new Color(82,82,82);
+        drawTopMenu();
         frame.getContentPane().setBackground(backgroundColour);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
